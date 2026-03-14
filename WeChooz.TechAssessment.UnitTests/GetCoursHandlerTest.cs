5c26995face90627440177b40e47dc984c3a6217
yohan fairfort <yohan.fairfort@jaji.fr>
2026-03-14 16:24:24 +0100

    Refacto

--- WeChooz.TechAssessment.UnitTests/GetCoursHandlerTest.cs
+++ WeChooz.TechAssessment.UnitTests/GetCoursHandlerTest.cs
@@ -6,46 +6,64 @@ namespace WeChooz.TechAssessment.UnitTests;
 
 public class GetCoursHandlerTest
 {
+    private const string NomCours = "NomCours";
+    private const string CourteDescription = "CourteDescription";
+    private const string LongueDescription = "LongueDescription";
+    private const int Duree = 2;
+    private const PopulationCibleEnum PopulationCibleEnum = UnitTests.PopulationCibleEnum.Elu;
+    private const int CapaciteMaximal = 10;
+    private const string NomFormateur = "NomFormateur";
+    private const string PrenomFormateur = "PrenomFormateur";
+    private readonly ICoursRepository? _coursRepository = Substitute.For<ICoursRepository>();
+    private GetCoursQueryHandler _handler;
+
+    public GetCoursHandlerTest()
+    {
+        _handler = new GetCoursQueryHandler(_coursRepository);
+    }
     [Fact]
     public async Task Should_Handle_return_les_cours()
     {
-        var coursRepository = Substitute.For<ICoursRepository>();
-        var coursReadModels = new List<CoursReadModel>()
-        {
-            new CoursReadModel()
-            {
-                Nom = "NomCours",
-                CourteDescription = "CourteDescription",
-                LongueDescription = "LongueDescription",
-                Duree = 2,
-                PopulationCible = PopulationCibleEnum.Elu,
-                CapaciteMaximal = 10,
-                Formateur = new PersonneReadModel
-                {
-                    Nom = "NomFormateur",
-                    Prenom = "PrenomFormateur"
-                }
-            }
-        };
-        coursRepository.GetAll(Arg.Any<CancellationToken>()).Returns(coursReadModels);
-        var handler = new GetCoursQueryHandler(coursRepository);
+        var coursReadModels = CoursReadModels();
+        _coursRepository.GetAll(Arg.Any<CancellationToken>()).Returns(coursReadModels);
         var getCoursQuery = new GetCoursQuery();
 
-        var result = await handler.Handle(getCoursQuery, CancellationToken.None);
+        var result = await _handler.Handle(getCoursQuery, CancellationToken.None);
 
         result.Should().BeEquivalentTo(
         [
             new CoursQueryDto
             {
-                Nom = "NomCours",
-                CourteDescription = "CourteDescription",
-                LongueDescription = "LongueDescription",
-                Duree = 2,
-                PopulationCible = PopulationCibleEnum.Elu,
-                CapaciteMaximal = 10,
-                NomFormateur = "NomFormateur",
-                PrenomFormateur = "PrenomFormateur"
+                Nom = NomCours,
+                CourteDescription = CourteDescription,
+                LongueDescription = LongueDescription,
+                Duree = Duree,
+                PopulationCible = PopulationCibleEnum,
+                CapaciteMaximal = CapaciteMaximal,
+                NomFormateur = NomFormateur,
+                PrenomFormateur = PrenomFormateur
             }
         ]);
+    }
+
+    private static List<CoursReadModel> CoursReadModels()
+    {
+        return new List<CoursReadModel>()
+        {
+            new CoursReadModel()
+            {
+                Nom = NomCours,
+                CourteDescription = CourteDescription,
+                LongueDescription = LongueDescription,
+                Duree = Duree,
+                PopulationCible = PopulationCibleEnum,
+                CapaciteMaximal = CapaciteMaximal,
+                Formateur = new PersonneReadModel
+                {
+                    Nom = NomFormateur,
+                    Prenom = PrenomFormateur
+                }
+            }
+        };
     }
 }
