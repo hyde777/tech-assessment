125d64c0456f8c5c828415818b65a83004f421df
yohan fairfort <yohan.fairfort@jaji.fr>
2026-03-15 23:51:09 +0100

    Green

--- WeChooz.TechAssessment.Domain/Sessions/CreeSessionCommand/SessionCreateCommandHandler.cs
+++ WeChooz.TechAssessment.Domain/Sessions/CreeSessionCommand/SessionCreateCommandHandler.cs
@@ -4,13 +4,23 @@ namespace WeChooz.TechAssessment.UnitTests;
 
 public class SessionCreateCommandHandler
 {
+    private readonly ISessionRepository _sessionRepository;
+
     public SessionCreateCommandHandler(ISessionRepository sessionRepository)
     {
-        throw new NotImplementedException();
+        _sessionRepository = sessionRepository;
     }
 
-    public async Task<object> Handle(SessionCreateCommand command, CancellationToken cancellationToken)
+    public async Task<int> Handle(SessionCreateCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var idSession = await _sessionRepository.Add(new SessionCreateModel
+        {
+            IdCours = command.IdCours,
+            IdParticipants = command.IdParticipants,
+            ModeDelivrance = command.ModeDelivrance,
+            DateDebut = command.DateDebut,
+        }, cancellationToken);
+
+        return idSession;
     }
 }
