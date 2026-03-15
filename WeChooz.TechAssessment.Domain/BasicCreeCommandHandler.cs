c7ffd7ebb1fb850beaa889c67d1dd1043231c5b1
yohan fairfort <yohan.fairfort@jaji.fr>
2026-03-16 00:59:58 +0100

    Add basic generic command and query

---
+++ WeChooz.TechAssessment.Domain/BasicCreeCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using WeChooz.TechAssessment.Domain.Cours.CreeCoursCommand;
+
+namespace WeChooz.TechAssessment.Domain;
+
+public class BasicCreeCommandHandler<CreeCommand,CreeModel>
+{
+    private readonly ICreeRepository<CreeModel> _coursrepository;
+    private readonly IMap<CreeCommand, CreeModel> _creeCoursMapper;
+
+    public BasicCreeCommandHandler(ICreeRepository<CreeModel> coursrepository, IMap<CreeCommand, CreeModel> creeCoursMapper)
+    {
+        _coursrepository = coursrepository;
+        _creeCoursMapper = creeCoursMapper;
+    }
+
+    public async Task<int> Handle(CreeCommand command, CancellationToken cancellationToken)
+    {
+        var coursCreateModel = _creeCoursMapper.MapFrom(command);
+        var id = await _coursrepository.Add(coursCreateModel, cancellationToken);
+
+        return id;
+    }
+}
---
+++ WeChooz.TechAssessment.Domain/BasicQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using WeChooz.TechAssessment.Domain.Cours.CreeCoursCommand;
+
+namespace WeChooz.TechAssessment.Domain;
+
+public class BasicQueryHandler<Query, ReadModel, QueryDto>
+{
+    private readonly IReadRepository<ReadModel> _coursRepository;
+    private readonly IMap<ReadModel, QueryDto> _coursQueryDtoMapper;
+
+    public BasicQueryHandler(IReadRepository<ReadModel> coursRepository, IMap<ReadModel,  QueryDto> coursQueryDtoMapper)
+    {
+        _coursRepository = coursRepository;
+        _coursQueryDtoMapper = coursQueryDtoMapper;
+    }
+
+    public async Task<List<QueryDto>> Handle(Query coursQuery, CancellationToken cancellationToken)
+    {
+        var coursReadModels = await _coursRepository.GetAll(cancellationToken);
+        return coursReadModels.Select(x => _coursQueryDtoMapper.MapFrom(x)).ToList();
+    }
+}
